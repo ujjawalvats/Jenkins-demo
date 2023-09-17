9bdaa2ebaf3f414842f3866d5537cf33d8fe43fc
Ujjawal Vats <ujjawalvats11820@gmail.com>
2023-09-17 14:29:36 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello World, DevOps has arrived!" to the terminal window.
+        System.out.println("Hello World, DevOps has arrived!");
+    }
+
+}
